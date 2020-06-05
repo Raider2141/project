c55e6a2a14e9e7ae95ab9015104b3bf656151611
Raider2141 <54362518+Raider2141@users.noreply.github.com>
2020-06-05 22:37:44 +0300

    s

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+        //don't play audio for small damages
+        if (damage >= 10)
+            GetComponent<AudioSource>().Play();
+        //decrease health according to magnitude of the object that hit us
+        Health -= damage;
+        //if health is 0, destroy the block
+        if (Health <= 0) Destroy(this.gameObject);
+    }
+
+    public float Health = 70f;
+
+
+}
---
+++ Assets/Scripts/GameHelper.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHelper : MonoBehaviour
+{
+
+
+    public BirdsHelper SelectedBirdsHelper { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cour = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {   ///Нажали кнопку мыши
+            if (SelectedBirdsHelper == null)
+            {
+                Collider2D[] all = Physics2D.OverlapCircleAll((Vector2)cour, 0.1f);
+
+                foreach (var item in all)
+                {
+                    if (item.GetComponent<BirdsHelper>())
+                    {
+                        SelectedBirdsHelper = item.GetComponent<BirdsHelper>();
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (SelectedBirdsHelper != null)
+        {
+
+            SelectedBirdsHelper.transform.position = Vector3.MoveTowards(
+                SelectedBirdsHelper.transform.position,
+                new Vector2(cour.x, cour.y),
+                Time.deltaTime * 10.0f);
+
+            var dir = SelectedBirdsHelper.StartPosition - new Vector2(SelectedBirdsHelper.transform.position.x, SelectedBirdsHelper.transform.position.y);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            SelectedBirdsHelper.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (SelectedBirdsHelper != null)
+            {
+                SelectedBirdsHelper.GetComponent<Rigidbody2D>().isKinematic = false;
+                SelectedBirdsHelper.GetComponent<Rigidbody2D>().AddForceAtPosition(
+                   SelectedBirdsHelper.transform.right * Vector3.Distance(SelectedBirdsHelper.transform.position, SelectedBirdsHelper.StartPosition) * 300,
+                    SelectedBirdsHelper.StartPosition
+                    );
+
+                ///Логика выстрела
+                SelectedBirdsHelper = null;
+            }
+        }
+    }
+}
